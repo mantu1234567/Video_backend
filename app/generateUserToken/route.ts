c4d716e02ef1f3a2a663a4434b4e3c5d6d088558
Mantu kumar <mantukumar803204@gmail.com>
2024-10-02 11:42:42 +0530

    first commit

---
+++ app/generateUserToken/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import { StreamClient,UserRequest} from '@stream-io/node-sdk';
+
+const apiKey =process.env.API_GETSTREAM_PUBLISHABLE_KEY;
+
+const secret =process.env.API_GETSTREAM_SECRET_KEY;
+console.log(apiKey);
+console.log("klk;lk;.klkl.l.l.");
+
+if(!apiKey || !secret){
+  throw new Error(
+    "Missing API key"
+  );
+}
+
+const client = new StreamClient(apiKey,secret);
+
+export async function POST(req:NextRequest){
+    const {userId,name,image,email} = await req.json();
+    const newUser : UserRequest={
+        id:userId,
+        role:"user",
+        name,
+        image,
+        custom:{
+            email,
+        },
+    };
+    await client.upsertUsers([newUser]);
+    const validity =60*60;
+    const token = client.generateUserToken({
+        user_id:userId,
+        validity_in_seconds:validity,
+    })
+    console.log(
+        `User ${userId} created with token ${token} and validity ${validity}`
+    );
+    return NextResponse.json({
+        token,
+    })
+}
